7970afa1a7ac7e595a004b555cedeebbb34a2a69
Erik M Jacobs <erikmjacobs@gmail.com>
2017-10-11 18:06:35 -0400

    Fixes bash variable interpolation

--- script.sh
+++ script.sh
@@ -3,7 +3,7 @@ set -x
 
 # start lab instance and store its details into a json file
 curl -s -X POST -H "Content-Type: application/json" \
--d '{ "auth_token": "$QWIKLAB_API_TOKEN", "focus""id": "70"} }' \
+-d '{ "auth_token": "'$QWIKLAB_API_TOKEN'", "focus""id": "70"} }' \
 https://redhat.qwiklab.com/api/v1/focuses/run_lab/ > lab_details.json
 
 echo "wait for lab instance to provision"
