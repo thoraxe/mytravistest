34c0a11dd27aeedc9359e0a83871d55e55f57416
Erik M Jacobs <erikmjacobs@gmail.com>
2017-10-11 17:59:37 -0400

    Stores details about lab into file

--- script.sh
+++ script.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -x
 
-# login to qwiklab and get token
-curl "$TEST_INSIDE_BASH"
-
-echo "start lab instance"
+# start lab instance and store its details into a json file
+curl -s -X POST -H "Content-Type: application/json" \
+-d '{ "auth_token": "$QWIKLAB_API_TOKEN", "focus""id": "70"} }' \
+https://redhat.qwiklab.com/api/v1/focuses/run_lab/ > lab_details.json
 
 echo "wait for lab instance to provision"
 
