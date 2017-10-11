5de7ae7511304c4b1bb12f3e5d5fdb41842e0352
Erik M Jacobs <erikmjacobs@gmail.com>
2017-10-11 10:50:21 -0400

    Put stages in script

--- script.sh
+++ script.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 set -x
-echo "i am a script that runs"
-[ 1 == 0 ]
+
+# login to qwiklab and get token
+curl google.com
+
+echo "start lab instance"
+
+echo "wait for lab instance to provision"
+
+echo "fetch SSH key for lab instance"
+
+echo "SSH into remote server and execute ansible"
+
+echo "tear down lab instance"
